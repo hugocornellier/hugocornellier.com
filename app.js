64c87290f210ed6b4333591e6e20f15995f62ef2
hugocornellier <hcornell@unb.ca>
2023-01-03 11:21:32 -0400

    Ports

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
+const req = require("express/lib/request");
 const app = express();
-const port = 5000;
+const port = 169;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {        
 	res.sendFile(__dirname + "/hugocornellier.html"); 
@@ -9,6 +10,12 @@ app.get('/', (req, res) => {
 app.get('/projects/orc-rush-tower-defense', (req, res) => {
 	res.sendFile(__dirname + "/projects/orc-rush-tower-defense/index.html");
 });
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`)
-);
+if (req.headers['x-forwarded-for'] == null) {
+	app.listen(3000, () =>
+		console.log(`Example app listening on port 3000!`)
+	);
+} else {
+	app.listen(5000, () =>
+		console.log(`Example app listening on port 5000!`)
+	);
+}
