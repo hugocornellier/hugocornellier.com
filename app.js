c28c4a8a849be0159bb910ce64d71f98bb0971d3
hugocornellier <hcornell@unb.ca>
2023-01-03 11:36:12 -0400

    Ports

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+let port = app.settings['views'].substring(0, 5) === "/User" ? 3000 : 5000;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/hugocornellier.html"); 
@@ -7,11 +8,6 @@ app.get('/', (req, res) => {
 app.get('/projects/orc-rush-tower-defense', (req, res) => {
 	res.sendFile(__dirname + "/projects/orc-rush-tower-defense/index.html");
 });
-console.log(app);
-if (app.address) {
-	app.listen(3000, () =>
-		console.log(`Example app listening on port 3000!`)
-	);
-} else {
-	console.log("Not local.");
-}
+app.listen(port, () =>
+	console.log(`Example app listening on port `+port)
+);
