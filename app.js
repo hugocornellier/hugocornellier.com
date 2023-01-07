d8167348a436998ccb857e0a301753d14d68d917
hugocornellier <hugo.cornellier@gmail.com>
2023-01-07 17:06:23 -0400

    Resto project.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/projects/orc-rush-tower-defense', (req, res) => {
 	res.sendFile(__dirname + "/projects/orc-rush-tower-defense/index.html");
 });
+app.get('/projects/resto', (req, res) => {
+	res.sendFile(__dirname + "/projects/resto/index.html");
+});
 app.listen(port, () =>
 	console.log(`Example app listening on port `+port)
 );
