39bdef9e4f22d3e65c39f54f6ea1ad39765a1474
hugocornellier <hugo.cornellier@gmail.com>
2023-01-07 21:45:19 -0400

    MongoDB

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
 const mongodb = require("mongodb")
+const axios = require('axios')
 const MongoClient = mongodb.MongoClient
 const date = new Date()
 const IP = require('ip')
+const geoip = require('geoip-lite')
 
 // Local env port to 3000 & live env port to 5000
 let port = app.settings['views'].substring(0, 5) === "/User" ? 3000 : 5000
 let ip = null
 
+const sendAPIRequest = async (ipAddress) => {
+	const apiResponse = await axios.get(URL + "&ip_address=" + ipAddress);
+	return apiResponse.data;
+}
+
 app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-	const ipAddress = IP.address();
-	let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-	console.log(ipAddress)
+app.get('/', async (req, res) => {
+	ip = IP.address()
+	insertViewToDb()
+	console.log(ip)
+	let geo = geoip.lookup(ip);
+	console.log(geo);
 	res.sendFile(__dirname + "/hugocornellier.html")
 })
 app.get('/projects/orc-rush-tower-defense', (req, res) => {
@@ -26,18 +35,21 @@ app.listen(port, () =>
 	console.log(`Example app listening on port `+port)
 )
 
-const connectionURL = "mongodb+srv://root:DoPgkVgBN6goWw3r@cluster0.g6od2xj.mongodb.net/?retryWrites=true&w=majority";
-const dbName = "mydb"
-let db = null
-MongoClient.connect(connectionURL,{
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-},(err,connectedClient) => {
-	if (err) throw err
-	db = connectedClient.db(dbName)
-	let entry = { ip: "100.100.100.1", city: "Fredericton", country: "Canada", date: date }
-	db.collection("user-views").insertOne(entry, function(err, res) {
+function insertViewToDb() {
+	const connectionURL = "mongodb+srv://root:DoPgkVgBN6goWw3r@cluster0.g6od2xj.mongodb.net/?retryWrites=true&w=majority";
+	const dbName = "mydb"
+	let db = null
+	MongoClient.connect(connectionURL,{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},(err,connectedClient) => {
 		if (err) throw err
-		console.log("1 document inserted")
+		db = connectedClient.db(dbName)
+		let entry = { ip: ip, city: "Fredericton", country: "Canada", date: date }
+		db.collection("user-views").insertOne(entry, function(err, res) {
+			if (err) throw err
+			console.log("1 document inserted")
+		})
 	})
-})
+}
+
