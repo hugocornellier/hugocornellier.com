6f81c53fce85d6c0c43b618c923f99b44de273a4
hugocornellier <hugo.cornellier@gmail.com>
2023-01-08 14:54:03 -0400

    MongoDB

--- app.js
+++ app.js
@@ -30,33 +30,35 @@ function insertViewToDb(ip) {
 		useUnifiedTopology: true
 	},(err, connectedClient) => {
 		if (err) throw err
-		let db = connectedClient.db(dbName)
-		let collection = "views"
+		let views = connectedClient.db(dbName).collection("views")
 
 		// Insert view only if IP hasn't already been seen within the last 30 minutes
-		db.collection(collection).find({
+		views.find({
 			ip: ip,
 			date: { "$gte" : moment().subtract(30, 'minute').toDate() }
 		}).toArray(function(err, result) {
-			if (err) throw err
-			if (result.length === 0) {
-				let url = "http://api.ipstack.com/" + ip + "?access_key=a2da89892582edff06d9bcba1fefe77e"
-				request({url: url}, function(err, response, body) {
-					if (err) { console.log(err); return; }
-					let parsed = JSON.parse(body)
-					let entry = {
-						ip: ip,
-						city: parsed.city,
-						province: parsed.region_name,
-						country: parsed.country_name,
-						date: new Date()
-					}
-					db.collection(collection).insertOne(entry, function(err, res) {
-						if (err) throw err
-						console.log("1 document inserted")
-					})
+			if (err)
+				throw err
+			if (result.length > 0) {
+				console.log("Not inserting view. This IP was seen within the last 30 minutes.")
+				return true
+			}
+			let url = "http://api.ipstack.com/" + ip + "?access_key=a2da89892582edff06d9bcba1fefe77e"
+			request({url: url}, function(err, response, body) {
+				if (err) { console.log(err); return; }
+				let parsed = JSON.parse(body)
+				let entry = {
+					ip: ip,
+					city: parsed.city,
+					province: parsed.region_name,
+					country: parsed.country_name,
+					date: new Date()
+				}
+				views.insertOne(entry, function(err, res) {
+					if (err) throw err
+					console.log("1 document inserted")
 				})
-			} else console.log("Not inserting view. This IP was seen within the last 30 minutes.")
+			})
 		})
 	})
 }
