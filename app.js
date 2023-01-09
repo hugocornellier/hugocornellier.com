d37a41f0d4eabb748d007abb95aac8d3248f0441
hugocornellier <hugo.cornellier@gmail.com>
2023-01-09 10:11:23 -0400

    Create logger module for views/ip/geolocation tracking

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const app = express()
-const mongodb = require("mongodb")
-const request = require('request')
-const moment = require('moment')
 const requestIp = require('request-ip')
-const MongoClient = mongodb.MongoClient
-const connectionURL = "mongodb+srv://root:DoPgkVgBN6goWw3r@cluster0.g6od2xj.mongodb.net/?retryWrites=true&w=majority"
-const dbName = "mydb"
+const logger = require('./js/logger.js');
 
 app.use(express.static(__dirname))
 app.set('trust proxy', 'loopback')
 app.get('/', async (req, res) => {
-	insertViewToDb(requestIp.getClientIp(req))
+	logger.insertViewToDb(requestIp.getClientIp(req))
 	res.sendFile(__dirname + "/hugocornellier.html")
 })
 app.get('/projects/*', (req, res) => {
@@ -23,43 +18,4 @@ let port = app.settings['views'].substring(0, 5) === "/User" ? 3000 : 5000
 app.listen(port, () =>
 	console.log(`Example app listening on port ` + port)
 )
-
-function insertViewToDb(ip) {
-	MongoClient.connect(connectionURL,{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},(err, connectedClient) => {
-		if (err) throw err
-		let views = connectedClient.db(dbName).collection("views")
-
-		// Insert view only if IP hasn't already been seen within the last 30 minutes
-		views.find({
-			ip: ip,
-			date: { "$gte" : moment().subtract(30, 'minute').toDate() }
-		}).toArray(function(err, result) {
-			if (err)
-				throw err
-			if (result.length > 0) {
-				console.log("Not inserting view. This IP was seen within the last 30 minutes.")
-				return true
-			}
-			let url = "http://api.ipstack.com/" + ip + "?access_key=a2da89892582edff06d9bcba1fefe77e"
-			request({url: url}, function(err, response, body) {
-				if (err) { console.log(err); return; }
-				let parsed = JSON.parse(body)
-				let entry = {
-					ip: ip,
-					city: parsed.city,
-					province: parsed.region_name,
-					country: parsed.country_name,
-					date: new Date()
-				}
-				views.insertOne(entry, function(err, res) {
-					if (err) throw err
-					console.log("1 document inserted")
-				})
-			})
-		})
-	})
-}
 
---
+++ js/logger.js
@@ -0,0 +1,44 @@
+const {MongoClient} = require("mongodb");
+const moment        = require("moment");
+const request       = require("request");
+const connectionURL = "mongodb+srv://root:DoPgkVgBN6goWw3r@cluster0.g6od2xj.mongodb.net/?retryWrites=true&w=majority"
+const dbName        = "mydb"
+
+/* Log user view including IP, city name, country and region
+   Insert view only if IP hasn't already been seen within the last 30 minutes */
+module.exports = {
+    insertViewToDb: function insertViewToDb(ip) {
+    MongoClient.connect(connectionURL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },(err, client) => {
+        if (err) { console.log(err); return; }
+        let views = client.db(dbName).collection("views")
+        views.find({
+            ip: ip,
+            date: { "$gte" : moment().subtract(30, 'minute').toDate() }
+        }).toArray(function(err, result) {
+            if (err) { console.log(err); return; }
+            if (result.length > 0) {
+                console.log("Not inserting view. This IP was seen within the last 30 minutes.")
+                return true
+            }
+            let url = "http://api.ipstack.com/" + ip + "?access_key=a2da89892582edff06d9bcba1fefe77e"
+            request({url: url}, function(err, response, body) {
+                if (err) { console.log(err); return; }
+                let data = JSON.parse(body)
+                views.insertOne({
+                    ip: ip,
+                    city: data.city,
+                    province: data.region_name,
+                    country: data.country_name,
+                    date: new Date()
+                }, function(err) {
+                    if (err) throw err
+                    console.log("1 document inserted")
+                })
+            })
+        })
+    })
+}
+}
