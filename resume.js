18f45f1f19c016817a9f675db954fab8dbdecd7c
hugocornellier <hugo.cornellier@gmail.com>
2023-01-09 10:53:27 -0400

    Formatting

--- resume.js
+++
@@ -1,8 +0,0 @@
-for (let p of document.getElementsByClassName('project-icon')) {
-    p.addEventListener("click", function() {
-        openInNewTab(p.getAttribute('data-url'));
-    });
-}
-function openInNewTab(url) {
-    window.open(url, '_blank').focus();
-}
