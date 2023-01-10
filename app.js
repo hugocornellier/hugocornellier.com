8c4be9120899fba01c380a1ec81514cb2da09eb2
hugocornellier <hugo.cornellier@gmail.com>
2023-01-10 11:49:56 -0400

    Fix file structure

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 const requestIp = require('request-ip')
-const logger = require('./js/logger.js');
+const logger = require('./client/js/logger.js');
 
 app.use(express.static(__dirname))
 app.get('/', async (req, res) => {
 	logger.insertViewToDb(requestIp.getClientIp(req))
-	res.sendFile(__dirname + "/hugocornellier.html")
+	res.sendFile(__dirname + "/client/hugocornellier.html")
 })
 app.get('/projects/*', (req, res) => {
-	res.sendFile(__dirname + "/projects/*/index.html")
+	res.sendFile(__dirname + "/client/projects/*/index.html")
 })
 
 // Local env port to 3000 & live env port to 5000
