67acd507c75f0cce5e405a777cccec5d6d631ba7
hugocornellier <hugo.cornellier@gmail.com>
2023-04-26 13:16:41 -0300

    Buailder

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const requestIp = require('request-ip')
-const logger = require('./client/js/logger.js');
+const logger = require('./client/js/logger.js')
 
 app.use(express.static(__dirname))
 app.get('/', async (req, res) => {
@@ -12,8 +16,14 @@ app.get('/projects/*', (req, res) => {
 	res.sendFile(__dirname + "/client/projects/*/index.html")
 })
 
+io.on('connection', (socket) => {
+	socket.on('image', (dataRefs) => {
+		console.log("User has uploaded new image");
+	});
+});
+
 // Local env port to 3000 & live env port to 5000
 let port = app.settings['views'].substring(0, 5) === "/User" ? 3000 : 5000
-app.listen(port, () =>
+server.listen(port, () =>
 	console.log(`Example app listening on port ` + port)
 )
--- projects/buailder/js/main.js
+++ projects/buailder/js/main.js
@@ -1,63 +1,246 @@
-const player_div =  document.getElementById('player')
-const BOARD_HEIGHT = 800
-const BOARD_WIDTH = 1200
-let player = {}
-player.jumping = false
-player.left = 0
-player.size = 20
-player.top = BOARD_HEIGHT - player.size
+/* Bootstrap 5 JS included */
 
-// Store active keys in keys dict
-let keys = {}
-document.onkeyup = e => keys[e.code] = false
-document.onkeydown = e => keys[e.code] = true
+let imageOriginal = new MarvinImage();
+let socket = io();
 
-// Generate new blocks
-// First, generate initial block, then generate a new one every 5s
-let blocksArr = []
-newBlock()
-setInterval(() => newBlock(), 5000)
+const overlayEl = document.getElementById('overlay');
 
-function newBlock() {
-    let nb = {}
-    nb.top = 0
-    nb.left = Math.floor(Math.random() * BOARD_WIDTH)
-    nb.size = 120
-    blocksArr.push(nb)
-    let newBlock = "<div data-id='" + (blocksArr.length - 1) + "' " +
-        "class='block in_air' " +
-        "style='width: 120px; top: 0; left: " + nb.left + "px'></div>"
-    document.getElementById("game").insertAdjacentHTML("beforeend", newBlock)
-}
+('use strict');
 
-// Update game board
-setInterval(move, 50)
-setInterval(() => {
-    player_div.style.setProperty("left", player.left + "px")
-    player_div.style.setProperty("top", player.top + "px")
-    for (let b of document.getElementsByClassName("in_air")) {
-        let curr = blocksArr[parseInt(b.getAttribute("data-id"))]
-        curr.top++
-        if (collisionDetect(curr, "down")) b.classList.remove("in_air")
-        b.style.setProperty("top",  parseInt(curr.top) + "px")
+
+// Drag and drop - single or multiple image files
+// https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+// https://codepen.io/joezimjs/pen/yPWQbd?editors=1000
+(function () {
+
+    'use strict';
+
+    // Four objects of interest: drop zones, input elements, gallery elements, and the files.
+    // dataRefs = {files: [image files], input: element ref, gallery: element ref}
+
+    const preventDefaults = event => {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+
+    const highlight = event =>
+        event.target.classList.add('highlight');
+
+    const unhighlight = event =>
+        event.target.classList.remove('highlight');
+
+    const getInputAndGalleryRefs = element => {
+        const zone = element.closest('.upload_dropZone') || false;
+        const gallery = zone.querySelector('.upload_gallery') || false;
+        const input = zone.querySelector('input[type="file"]') || false;
+        return {input: input, gallery: gallery};
     }
-}, 10)
 
-function collisionDetect(obj, dir) {
-    if (dir === "down") {
-        if (BOARD_HEIGHT - obj.size - obj.top <= 4) {
-            obj.top = BOARD_HEIGHT - obj.size
-            return true
-        }
-        for (let p of document.getElementsByClassName("block")) {
-            let vd = obj.top - parseInt(p.style.top) + obj.size
-            if (vd <= 4 && vd > -1
-                    && obj.left >= parseInt(p.style.left) - obj.size
-                    && obj.left <= (parseInt(p.style.left) + parseInt(p.style.width))) {
-                if (vd !== 0) obj.top = parseInt(p.style.top) - obj.size
-                return true
+    const handleDrop = event => {
+        const dataRefs = getInputAndGalleryRefs(event.target);
+        dataRefs.files = event.dataTransfer.files;
+        handleFiles(dataRefs);
+    }
+
+    const eventHandlers = zone => {
+
+        const dataRefs = getInputAndGalleryRefs(zone);
+        if (!dataRefs.input) return;
+
+        // Prevent default drag behaviors
+        ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(event => {
+            zone.addEventListener(event, preventDefaults, false);
+            document.body.addEventListener(event, preventDefaults, false);
+        });
+
+        // Highlighting drop area when item is dragged over it
+        ;['dragenter', 'dragover'].forEach(event => {
+            zone.addEventListener(event, highlight, false);
+        });
+        ;['dragleave', 'drop'].forEach(event => {
+            zone.addEventListener(event, unhighlight, false);
+        });
+
+        // Handle dropped files
+        zone.addEventListener('drop', handleDrop, false);
+
+        // Handle browse selected files
+        dataRefs.input.addEventListener('change', event => {
+            dataRefs.files = event.target.files;
+            handleFiles(dataRefs);
+        }, false);
+
+    }
+
+    // Initialise ALL dropzones
+    const dropZones = document.querySelectorAll('.upload_dropZone');
+    for (const zone of dropZones) {
+        eventHandlers(zone);
+    }
+
+    // No 'image/gif' or PDF or webp allowed here, but it's up to your use case.
+    // Double checks the input "accept" attribute
+    const isImageFile = file =>
+        ['image/jpeg', 'image/png', 'image/svg+xml'].includes(file.type);
+
+    function previewFiles(dataRefs) {
+        if (!dataRefs.gallery) return;
+        for (const file of dataRefs.files) {
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = function() {
+                let img = document.createElement('img');
+                img.className = 'upload_img';
+                img.setAttribute('alt', file.name);
+                img.src = reader.result;
+                document.getElementById('image_wrap').appendChild(img);
+                document.getElementById('form').style.display = "none";
+                imageOriginal.load(reader.result, imageLoaded);
             }
         }
     }
-    return false
-}
+    
+    const getGroups = clump => {
+        let groups = []
+        let prev_index = null
+        for (const member of clump) {
+            let index = member.Id
+            if (765 - member.sum > 50) {
+                if (!(prev_index == null || index - prev_index !== 1)) {
+                    if (index - prev_index === 1) {
+                        groups[groups.length - 1].push(index)
+                    }
+                } else {
+                    let confirmed = []
+                    confirmed.push(index)
+                    groups.push(confirmed)
+                }
+                prev_index = index
+            }
+        }
+        return groups
+    }
+
+    // Return average of RGB data for all pixels in every given row
+    const getRowAveragesRGB = imageDim => {
+        const rows = []
+        for (let y = 0; y < imageDim.imageHeight; y++) {
+            const row_rgb = { r: 0, g: 0, b: 0 }
+            for (let x = 0; x < imageDim.imageWidth; x++) {
+                row_rgb.r += imageOriginal.getIntComponent0(x, y)
+                row_rgb.g += imageOriginal.getIntComponent1(x, y)
+                row_rgb.b += imageOriginal.getIntComponent2(x, y)
+            }
+            row_rgb.r /= imageDim.imageWidth;
+            row_rgb.g /= imageDim.imageWidth;
+            row_rgb.b /= imageDim.imageWidth;
+            let sum = row_rgb.r + row_rgb.g + row_rgb.b
+            rows.push({sum, Id: y, r: row_rgb.r, g: row_rgb.g, b: row_rgb.b})
+        }
+        return rows
+    }
+
+    const findHorizLines = x => {
+        const imageDim = {imageHeight:imageOriginal.getHeight(), imageWidth:imageOriginal.getWidth()};
+        let newHeight = Math.round(500 * (imageDim.imageHeight / imageDim.imageWidth));
+        document.getElementById('image_wrap').style.display = "block"
+        overlayEl.style.display = "block"
+        overlayEl.style.height = newHeight + "px"
+
+        const rows_rgb = getRowAveragesRGB(imageDim)
+        let groups = getGroups(rows_rgb)
+        console.log(groups)
+
+        for (const group of groups) {
+            let columns = [];
+            let clump = [];
+            let i = 0;
+            let startRow = imageDim.imageHeight;
+            let endRow = 0;
+            for (const ID of group) {
+                if (ID < startRow) startRow = ID
+                if (ID > endRow) endRow = ID
+                let y = ID;
+                i++;
+                for (let x = 0; x < imageDim.imageWidth; x++) {
+                    if (columns[x] == null) {
+                        columns[x] = imageOriginal.getIntComponent0(x,y);
+                    } else {
+                        columns[x] += imageOriginal.getIntComponent0(x,y);
+                    }
+                    columns[x] += imageOriginal.getIntComponent1(x,y);
+                    columns[x] += imageOriginal.getIntComponent2(x,y);
+                    if (i === group.length) {
+                        columns[x] /= group.length
+                        let sum = columns[x];
+                        let Id = x;
+                        clump.push({sum, Id})
+                    }
+                }
+            }
+
+            let colGroups = getGroups(clump);
+            let startCol = imageDim.imageWidth;
+            let endCol = 0;
+            for (const ID of colGroups) {
+                for (const i of ID) {
+                    if (i < startCol) startCol = i
+                    if (i > endCol) endCol = i
+                }
+            }
+
+            let line_data = [];
+            line_data.startRow = startRow;
+            line_data.endRow = endRow;
+            line_data.startCol = startCol;
+            line_data.endCol = endCol;
+
+            // Draw overlay marker
+            addOverlayLine(line_data);
+        }
+    }
+
+    const imageLoaded = x => {
+        findHorizLines(); // Search for (and overlay) horizontal lines
+    }
+
+    const addOverlayLine = line_data => {
+        console.log("There is a line => from rows (" + line_data.startRow + ", " + line_data.endRow +
+                                         "), cols (" + line_data.startCol + ", " + line_data.endCol + ")")
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', "overlay_line");
+        newDiv.style.height = line_data.endRow - line_data.startRow + "px";
+        newDiv.style.width = line_data.endCol - line_data.startCol + "px";
+        newDiv.style.position = "relative";
+        newDiv.style.top = line_data.startRow + "px";
+        newDiv.style.left = line_data.startCol + "px";
+        overlayEl.appendChild(newDiv);
+    }
+
+    const imageUpload = dataRefs => {
+        console.log("User has uploaded an image")
+        console.log(dataRefs)
+        socket.emit('image', dataRefs)
+    }
+
+    // Handle both selected and dropped files
+    const handleFiles = dataRefs => {
+
+        let files = [...dataRefs.files];
+
+        // Remove unaccepted file types
+        files = files.filter(item => {
+            if (!isImageFile(item)) {
+                console.log('Not an image, ', item.type);
+            }
+            return isImageFile(item) ? item : null;
+        });
+
+        if (!files.length) return;
+        dataRefs.files = files;
+
+        previewFiles(dataRefs);
+        imageUpload(dataRefs);
+    }
+
+})();
