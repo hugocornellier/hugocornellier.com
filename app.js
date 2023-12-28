dbb8a9e78cc764a7d3067db6b5aefb0e0befe8c5
hugocornellier <hugo.cornellier@gmail.com>
2023-12-28 11:21:44 -0400

    Updates.

--- app.js
+++ app.js
@@ -9,46 +9,18 @@ app.use(cors({
 }));
 const server = http.createServer(app)
 const io = new Server(server)
-const { MongoClient } = require("mongodb");
-let pw = encodeURIComponent("+z:~hu2z._pC98u")
-const uri = `mongodb+srv://hugocornellier:${pw}leaderboard.qrubtzl.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(uri)
+//const { MongoClient } = require("mongodb");
+//let pw = encodeURIComponent("+z:~hu2z._pC98u")
+//const uri = `mongodb+srv://hugocornellier:${pw}leaderboard.qrubtzl.mongodb.net/?retryWrites=true&w=majority`
+//const client = new MongoClient(uri)
 
 app.use(express.static(__dirname))
 app.get('/', async (req, res) => {
 	res.sendFile(__dirname + "/client/hugocornellier.html")
 })
-app.get('/dealer_portal', async (req, res) => {
-	res.sendFile(__dirname + "/projects/dealer_portal/index.html")
-})
-app.get('/api', async (req, res) => {
-	let userIP =  req.originalUrl.split("ip=")[1]
-	request('http://ip-api.io/api/json/' + userIP)
-	.then(response => {
-		response = JSON.parse(response)
-		console.log(response)
-		res.json({
-			country: response['country_name'],
-			province: response['region_name'],
-			city: response['city']
-		})
-	})
-	.catch(err => console.log(err))
-})
 app.get('/projects/*/', (req, res) => {
 	res.sendFile(__dirname + "/projects/*/index.html")
 })
-
-async function run(name, email, socket) {
-	const database = client.db("insertDB")
-	const giveaway_entries = database.collection("giveaway_entries")
-	const doc = {
-		email: email,
-		name: name,
-	}
-	const result = await giveaway_entries.insertOne(doc)
-	socket.emit('entry_added', result)
-}
 
 io.on('connection', (socket) => {
 	socket.on('join_leaderboard', async (data) => {
@@ -63,21 +35,6 @@ io.on('connection', (socket) => {
 		}
 		const result = await giveaway_entries.insertOne(doc)
 		socket.emit('entry_added', result)
-	})
-	socket.on('giveaway_entry', (data) => {
-		console.log('data: ' + data);
-		run(data[0], data[1], socket).catch(console.dir);
-	})
-	socket.on('conn', () => {
-		fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => {
-			request('http://ip-api.io/api/json/' + data)
-				.then(response => {
-					response = JSON.parse(response)
-					console.log(response)
-					socket.emit('result', response['country_name'], response['region_code'], response['region_name'])
-				})
-				.catch(err => console.log(err))
-		})
 	})
 })
 
