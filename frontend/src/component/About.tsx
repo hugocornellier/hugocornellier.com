b29dbd47886ac997d449595cee52d80e3556f92a
hugocornellier <hugo.cornellier@gmail.com>
2024-03-25 14:10:18 -0300

    Added "Terminal" typer!

--- frontend/src/component/About.tsx
+++ frontend/src/component/About.tsx
@@ -2,32 +2,28 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import pdf from '../data/CV.pdf'
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Terminal from "./Terminal";
 
 const About = () => {
     return (
         <section className="resume-section p-3 p-lg-5 d-flex d-column" id="about">
-            <div className="my-auto">
+            <div className="my-auto w-full">
                 <h1 className="mb-0">Hugo <span className="text-primary">Cornellier</span></h1>
                 <h3>
                     Full-Stack Developer
                 </h3>
-                <div className="subheading mb-5">Fredericton, Canada · <a href="mailto:hugo.cornellier@gmail.com">hugo.cornellier@gmail.com</a></div>
-                <p className="about-me mb-5 max-w-0">
-                    <p>My name is Hugo Cornellier, and I am a passionate and driven developer. I am currently
-                        working as a Full-Stack Developer at Off-Grid Distribution.</p>
-                    <p>I have a B. Sc. in Computer Science from UNB, graduating in August 2023 in the First Division.
-                        I'm proud of my academic record: I received a final GPA of 3.7, and earned a spot on the Dean's
-                        List in 2023.</p>
-                </p>
+                <div className="subheading mb-1">Fredericton, Canada · <a
+                    href="mailto:hugo.cornellier@gmail.com">hugo.cornellier@gmail.com</a></div>
+                <div className="d-flex flex-row social-icons mb-5">
+                    <SocialIcon link="https://linkedin.com/in/hugocornellier" icon={faLinkedin}/>
+                    <SocialIcon link="https://github.com/hugocornellier" icon={faGithub}/>
+                </div>
+                <Terminal/>
                 <a href={pdf} target="_blank" rel="noopener noreferrer">
                     View Resume as PDF
                 </a>
-                <br />
-                <br />
-                <div className="d-flex flex-row social-icons">
-                    <SocialIcon link="https://linkedin.com/in/hugocornellier" icon={faLinkedin} />
-                    <SocialIcon link="https://github.com/hugocornellier" icon={faGithub} />
-                </div>
+                <br/>
+                <br/>
             </div>
         </section>
     );
@@ -38,7 +34,7 @@ interface SocialIconProps {
     icon: any; // or FontAwesomeIconProps
 }
 
-const SocialIcon: React.FC<SocialIconProps> = ({ link, icon }) => {
+const SocialIcon: React.FC<SocialIconProps> = ({link, icon}) => {
     return (
         <a href={link} target="_blank" rel="noopener noreferrer" className="d-flex justify-content-center align-items-center rounded-circle social-icon project-icon">
             <FontAwesomeIcon icon={icon} />
---
+++ frontend/src/component/Terminal.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from 'react';
+import { Typed } from "react-typed";
+import "../styles/terminal.css";
+
+const TerminalComponent = () => {
+    const terminalRef = useRef(null);
+
+    useEffect(() => {
+        const terminalTyped = new Typed(terminalRef.current, {
+            strings: [
+                `My name is Hugo Cornellier, and I am a passionate and driven full-stack developer.
+                <br/><br/>
+                While at Off-Grid Distributions and Bulletproof, I’ve built my web development skills using technologies 
+                such as React, TypeScript, HTML5/CSS, along with libraries such as Tailwind. I am well-versed in both 
+                SQL and NoSQL, particularly MongoDB. I have strong communication skills, and I work well collaboratively 
+                or independently.
+                <br/><br/>
+                I am well versed in the latest software development methodologies - task estimation, prioritization, 
+                testing, code review, devops. My projects have involved both backend code optimization - where 
+                improvements are measured in milliseconds - and frontend user experience.
+                <br/><br/>
+                 Let's connect! My LinkedIn and Github profiles, as well as my email address, can be found above.`
+            ],
+            typeSpeed: 40,
+            backSpeed: 0,
+            backDelay: 3000,
+            loop: false,
+            onComplete: function (self) {
+                setTimeout(function () {
+                    self.reset();
+                }, 1000);
+            }
+        });
+
+        return () => {
+            terminalTyped.destroy();
+        };
+    }, []);
+
+    return (
+        <div className="terminal-outer">
+            <div className="terminal-container">
+                <div className="terminal-section terminal-header">
+                    <div className="terminal-controls">
+                        <span className="elem elem-close"></span>
+                        <span className="elem elem-minimize"></span>
+                        <span className="elem elem-fullscreen"></span>
+                    </div>
+                </div>
+                <div className="terminal-section terminal-body">
+                    <div className="terminal-section terminal-body-inner">
+                        <p>
+                            <span className="terminal-user">Hello! </span>
+                            <span ref={terminalRef}></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TerminalComponent;
